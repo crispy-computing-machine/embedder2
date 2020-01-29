42d4133b6f281ce711712d27989ea61eea5a8fb5
Stefan <stefan>
2020-01-29 12:22:51 +0000

    changed asset zip to contain %APPVEYOR_BUILD_FOLDER%

--- appveyor/build_task.cmd
+++ appveyor/build_task.cmd
@@ -57,7 +57,7 @@ setlocal enableextensions enabledelayedexpansion
 		rem 7z a embedder.zip C:\obj\Release_TS\*
 
 		rem embed.exe that was built
-		7z a embedder.zip C:\projects\embedder\*
+		7z a embedder.zip %APPVEYOR_BUILD_FOLDER%\*
 
 		appveyor PushArtifact embedder.zip -FileName embedder.zip
 	)
