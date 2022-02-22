e418a270c0cb1278c8208af0958cfee795af97cc
Stefan <5684066+crispy-computing-machine@users.noreply.github.com>
2022-02-22 10:05:07 +0000

    define('EMBEDED', 1);

--- src/embeder.cpp
+++ src/embeder.cpp
@@ -29,28 +29,23 @@ int main(int argc, char **argv)
 
     php_embed_module.php_ini_ignore = 0;
     php_embed_module.php_ini_path_override = "./php.ini";
-
     PHP_EMBED_START_BLOCK(argc,argv);
-
-    zend_try {
-        /* Try to execute something that will fail */
-        /* We are embeded */
-        zend_eval_string("define('EMBEDED', 1);", retval, "main" TSRMLS_CC);
-        // zend_alter_ini_entry("extension_dir", 14, dir, strlen(dir), PHP_INI_ALL, PHP_INI_STAGE_ACTIVATE);
-        // zend_alter_ini_entry("error_reporting", 16, "0", 1, PHP_INI_ALL, PHP_INI_STAGE_ACTIVATE);
-        retval = zend_eval_string(
-                                code,
-                                 NULL,
-                                  (char *)"" TSRMLS_CC
-                                  ) == SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
-     } zend_catch {
-         /* There was an error!
-         * Try a different line instead */
-        exit(retval);
-     } zend_end_try();
-
-
+        zend_try {
+            /* Try to execute something that will fail */
+            /* We are embeded */
+            zend_eval_string("define('EMBEDED', 1);", NULL, "main" TSRMLS_CC);
+            // zend_alter_ini_entry("extension_dir", 14, dir, strlen(dir), PHP_INI_ALL, PHP_INI_STAGE_ACTIVATE);
+            // zend_alter_ini_entry("error_reporting", 16, "0", 1, PHP_INI_ALL, PHP_INI_STAGE_ACTIVATE);
+            retval = zend_eval_string(
+                                    code,
+                                     NULL,
+                                      (char *)"" TSRMLS_CC
+                                      ) == SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+         } zend_catch {
+             /* There was an error!
+             * Try a different line instead */
+            exit(retval);
+         } zend_end_try();
     PHP_EMBED_END_BLOCK();
-
     exit(retval);
 }
