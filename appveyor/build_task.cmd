df0b76cca61ba56c9937b42787989ee4858cb0c6
Stefan <Stefan>
2022-08-26 12:03:49 +0100

    nmake snap

--- appveyor/build_task.cmd
+++ appveyor/build_task.cmd
@@ -34,16 +34,17 @@ setlocal enableextensions enabledelayedexpansion
 
 		cmd /c configure.bat --!ZTS_STATE!-zts --enable-object-out-dir=%PHP_BUILD_OBJ_DIR% --with-config-file-scan-dir=%APPVEYOR_BUILD_FOLDER%\build\modules.d --with-prefix=%APPVEYOR_BUILD_FOLDER%\build --with-php-build=%DEPS_DIR% --enable-embed --enable-snapshot-build --disable-debug-pack --enable-com-dotnet=shared --without-analyzer --with-php-build=%DEPS_DIR% %ADD_CONF% --disable-test-ini  ^
 		--with-bz2=static ^
-		--with-dba=static ^
-		--with-openssl=c:\OpenSSL-Win32
+		--with-dba=static
 
 		if %errorlevel% neq 0 exit /b 3
 
+        rem Suppress logo output of nmake
 		nmake /NOLOGO
 
 		if %errorlevel% neq 0 exit /b 3
 
-		nmake install
+        nmake snap
+		rem nmake install
 
 		if %errorlevel% neq 0 exit /b 3
 
