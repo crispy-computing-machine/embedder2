600672d5196a31e40de821fb25380d920b59df46
Crispy Computing Machine <5684066+crispy-computing-machine@users.noreply.github.com>
2022-08-27 14:39:58 +0100

    static?

--- appveyor/build_task.cmd
+++ appveyor/build_task.cmd
@@ -32,21 +32,20 @@ setlocal enableextensions enabledelayedexpansion
 
 		if %errorlevel% neq 0 exit /b 3
 
-		cmd /c configure.bat --!ZTS_STATE!-zts --enable-object-out-dir=%PHP_BUILD_OBJ_DIR% --with-config-file-scan-dir=%APPVEYOR_BUILD_FOLDER%\build\modules.d --with-prefix=%APPVEYOR_BUILD_FOLDER%\build --with-php-build=%DEPS_DIR% --enable-embed --enable-snapshot-build --disable-debug-pack --enable-com-dotnet=shared --without-analyzer --with-php-build=%DEPS_DIR% %ADD_CONF% --disable-test-ini  ^
-		--with-bz2=static ^
-		--with-dba=static ^
-		--with-curl=static ^
-		--with-enchant=static ^
-		--with-exif=static ^
-		--with-ffi=static ^
-		--with-fileinfo=static ^
-		--with-filter=static ^
-		--with-ftp=static ^
-		--with-gd=static ^
-		--with-gettext=static ^
-		--with-gmp=static
+		 cmd /c configure.bat --!ZTS_STATE!-zts --enable-object-out-dir=%PHP_BUILD_OBJ_DIR% --with-config-file-scan-dir=%APPVEYOR_BUILD_FOLDER%\build\modules.d --with-prefix=%APPVEYOR_BUILD_FOLDER%\build --with-php-build=%DEPS_DIR% ^
+		--enable-snapshot-build ^
+		--enable-static= ^
+		--enable-embed ^
+		--disable-debug-pack ^
+		--enable-com-dotnet=shared ^
+		--without-analyzer
+		--disable-test-ini  ^
+		--config-cache ^
+		--disable-cgi ^
+		--enable-com-dotnet
+		rem --with-parallel --with-extra-libs=c:\build-cache\pthreads\lib --with-extra-includes=c:\build-cache\pthreads\include
 
-		
+
 		if %errorlevel% neq 0 exit /b 3
 
         rem Suppress logo output of nmake
