115db3302548aa6b206c2f7e728a5618ef2dd5ce
Crispy Computing Machine <5684066+crispy-computing-machine@users.noreply.github.com>
2022-08-27 16:45:54 +0100

    cleanup

--- clean.cmd
+++ clean.cmd
@@ -1,10 +1,4 @@
 @echo off
 
 echo Cleaning up directories
-rm -rf out/
-rm -rf src/Debug_console
-rm -rf "src/Release console"
-rm -rf src/out src/res/Thumbs.db
-rm -rf embeder2.exe
-rm -rf *.exe
-rm -rf *.dll
+
