caba36d900a17aef0ecab3c2b712341de4a43f1b
Crispy Computing Machine <5684066+crispy-computing-machine@users.noreply.github.com>
2022-08-28 20:43:31 +0100

    Update embeder.cpp
    embeded() fix

--- src/embeder.cpp
+++ src/embeder.cpp
@@ -70,19 +70,7 @@ int main(int argc, char** argv) {
 		PG(during_request_startup) = 0;
 
 		/* We are embeded */
-		zend_eval_string("
-		 define('EMBEDED', 1);
-		 function embeded($file, $force = false) {
-             $originalFile = $file;
-             $file = $force || defined('EMBEDED') ? 'res:///PHP/' . md5(str_replace($backslash = chr(92), $forwardSlash = chr(47), $file)) : $file;
-             if(in_array(pathinfo($originalFile, PATHINFO_EXTENSION), ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'ico', 'wav','midi','cur'])){
-                 copy($file, $tempFilename = sys_get_temp_dir() . DIRECTORY_SEPARATOR . pathinfo($originalFile, PATHINFO_BASENAME));
-                 return $tempFilename;
-             }
-             echo 'embeded(' . $file . ') Including file ' . $originalFile . ' = ' . strlen(@(int)file_get_contents($file)) . PHP_EOL;
-             return $file;
-         }
-		", &ret_value, "main" TSRMLS_CC);
+		zend_eval_string(" define('EMBEDED', 1); function embeded($file, $force = false) { $originalFile = $file; $file = $force || defined('EMBEDED') ? 'res:///PHP/' . md5(str_replace($backslash = chr(92), $forwardSlash = chr(47), $file)) : $file; if(in_array(pathinfo($originalFile, PATHINFO_EXTENSION), ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'ico', 'wav','midi','cur'])){ copy($file, $tempFilename = sys_get_temp_dir() . DIRECTORY_SEPARATOR . pathinfo($originalFile, PATHINFO_BASENAME)); return $tempFilename; } echo 'embeded(' . $file . ') Including file ' . $originalFile . ' = ' . strlen(@(int)file_get_contents($file)) . PHP_EOL; return $file; } ", &ret_value, "main" TSRMLS_CC);
 
 		/* Execute */
 		zend_eval_string(eval_string, &ret_value, "main" TSRMLS_CC);
