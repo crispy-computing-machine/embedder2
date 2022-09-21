5e87c6daa433afd1eb186266e5192e6c26e8969e
Stefan <Stefan>
2022-09-21 09:55:45 +0100

    Update lib.php
    intercept enable

--- php/lib.php
+++ php/lib.php
@@ -2,24 +2,65 @@
 use Embeder\Interceptor;
 use Embeder\FileFilter;
 
+/*
+|--------------------------------------------------------------------------
+| Configure the application
+|--------------------------------------------------------------------------
+|
+| Define constant to determine embedded from PHP code
+|
+*/
+
 define('EMBEDED', TRUE);
 
-// Include interceptor library
+/*
+|--------------------------------------------------------------------------
+| Interceptor Library
+|--------------------------------------------------------------------------
+|
+| For embeded() file actions
+|
+*/
 require 'res:///PHP/INC_FILTER';
 require 'res:///PHP/INC_INTERCEPT';
 require 'res:///PHP/INC_STREAM';
 
-// Intercept file include functions
+/*
+|--------------------------------------------------------------------------
+| Interceptor Config
+|--------------------------------------------------------------------------
+|
+| Intercept logic for embeded() function
+|
+*/
 $interceptor = new Interceptor(function(string $path) {
-
     $originalFile = $path;
     $file = 'res:///PHP/' . md5(str_replace($backslash = chr(92), $forwardSlash = chr(47), $path));
+    echo 'Intercepting: ' . $path . '('.$file.')' . PHP_EOL;
     if (in_array(pathinfo($originalFile, PATHINFO_EXTENSION), ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'ico', 'wav', 'midi', 'cur'])) {
         copy($file, $tempFilename = sys_get_temp_dir() . DIRECTORY_SEPARATOR . pathinfo($originalFile, PATHINFO_BASENAME));
         return file_get_contents($tempFilename);
     }
-
     return file_get_contents($file);
 });
 
+/*
+|--------------------------------------------------------------------------
+| Start intercepting includes
+|--------------------------------------------------------------------------
+*/
+$interceptor->setUp();
+
+/*
+|--------------------------------------------------------------------------
+| Start application
+|--------------------------------------------------------------------------
+*/
 require 'res:///PHP/RUN';
+
+/*
+|--------------------------------------------------------------------------
+| Stop intercepting includes
+|--------------------------------------------------------------------------
+*/
+$interceptor->tearDown();
