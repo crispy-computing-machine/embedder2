a6ad241392f5a180ea18878f74597469a0ee107b
Stefan <Stefan>
2022-09-21 11:21:22 +0100

    Update lib.php
    intercept *.php

--- php/lib.php
+++ php/lib.php
@@ -33,15 +33,35 @@ require 'res:///PHP/INC_STREAM';
 | Intercept logic for embeded() function
 |
 */
-$interceptor = new Interceptor(function(string $path) {
-    $originalFile = $path;
-    $file = 'res:///PHP/' . md5(str_replace($backslash = chr(92), $forwardSlash = chr(47), $path));
-    echo 'Intercepting: ' . $path . '('.$file.')' . PHP_EOL;
-    if (in_array(pathinfo($originalFile, PATHINFO_EXTENSION), ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'ico', 'wav', 'midi', 'cur'])) {
-        copy($file, $tempFilename = sys_get_temp_dir() . DIRECTORY_SEPARATOR . pathinfo($originalFile, PATHINFO_BASENAME));
-        return file_get_contents($tempFilename);
+$fileFilter = FileFilter::createAllWhitelisted();
+$fileFilter->whitelistExtension('php');
+
+// Media
+$fileFilter->whitelistExtension('bmp');
+$fileFilter->whitelistExtension('gif');
+$fileFilter->whitelistExtension('jpg');
+$fileFilter->whitelistExtension('jpeg');
+$fileFilter->whitelistExtension('png');
+$fileFilter->whitelistExtension('ico');
+$fileFilter->whitelistExtension('wav');
+$fileFilter->whitelistExtension('midi');
+$fileFilter->whitelistExtension('cur');
+
+$interceptor = new Interceptor(function(string $path) use($fileFilter) {
+
+    if($fileFilter->test($path)){
+        $originalFile = $path;
+        $file = 'res:///PHP/' . md5(str_replace($backslash = chr(92), $forwardSlash = chr(47), $path));
+        echo 'Intercepting: ' . $path . '('.$file.')' . PHP_EOL;
+        if (in_array(pathinfo($originalFile, PATHINFO_EXTENSION), ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'ico', 'wav', 'midi', 'cur'])) {
+            copy($file, $tempFilename = sys_get_temp_dir() . DIRECTORY_SEPARATOR . pathinfo($originalFile, PATHINFO_BASENAME));
+            return file_get_contents($tempFilename);
+        }
+        return file_get_contents($file);
+
     }
-    return file_get_contents($file);
+
+    return null;
 });
 
 /*
