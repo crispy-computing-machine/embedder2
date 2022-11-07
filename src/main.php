d4d629b628c833da370d6b27d2c44c501ab4ce65
Stefan <author@email.com>
2022-11-07 16:31:43 +0000

    test main.php include script

--- src/main.php
+++ src/main.php
@@ -1,2 +1,11 @@
 <?php
-include $argv[1];
+try {
+    echo $argv[1] . PHP_EOL;
+    if(file_exists($argv[1])){
+        include $argv[1];
+    } else{
+        echo 'No file provided...';
+    }
+} catch (Throwable $e){
+    die($e);
+}
