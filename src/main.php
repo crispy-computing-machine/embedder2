2747dea7bba5ee8259bef52d84695a908ad108f7
Stefan <author@email.com>
2022-11-07 17:25:07 +0000

    try/catch cuasing php warnings/errors? Unexpected character in input:  '↓' (ASCII=25) state=0

--- src/main.php
+++ src/main.php
@@ -1,11 +1,8 @@
 <?php
-try {
-    echo $argv[1] . PHP_EOL;
-    if(file_exists($argv[1])){
-        include $argv[1];
-    } else{
-        echo 'No file provided...';
-    }
-} catch (Throwable $e){
-    die($e);
+
+echo $argv[1] . PHP_EOL;
+if(file_exists($argv[1])){
+    include $argv[1];
+} else{
+    echo 'No file provided...';
 }
